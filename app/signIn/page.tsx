0d29c51ce661e6aeaaa32196e38f79a7535a72b4
Anant-Raj17 <anantraj.0051@gmail.com>
2024-07-24 18:58:15 +0530

    commit 17

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import Navbar from "../components/NavBar";
 
 export default function AuthPage() {
   const [email, setEmail] = useState("");
@@ -40,68 +41,71 @@ export default function AuthPage() {
   };
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      <div className="m-auto w-full max-w-md">
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h2 className="text-2xl font-bold mb-6">
-            {isLogin ? "Login" : "Register"}
-          </h2>
-          <p className="mb-4 text-gray-600">
-            {isLogin
-              ? "Join our community of plant enthusiasts and get personalized advice for your green friends!"
-              : "Create an account to join our plant-loving community!"}
-          </p>
-          <form onSubmit={handleAuth}>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-                required
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                required
-              />
-            </div>
-            <div className="flex items-center justify-between">
-              <button className="btn btn-primary w-full" type="submit">
-                {isLogin ? "Login" : "Register"}
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="text-center">
-          <button
-            className="text-primary hover:underline"
-            onClick={() => setIsLogin(!isLogin)}
-          >
-            {isLogin
-              ? "Don't have an account? Register"
-              : "Already have an account? Login"}
-          </button>
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <Navbar />
+      <div className="flex min-h-screen bg-gray-100">
+        <div className="m-auto w-full max-w-md">
+          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <h2 className="text-2xl font-bold mb-6">
+              {isLogin ? "Login" : "Register"}
+            </h2>
+            <p className="mb-4 text-gray-600">
+              {isLogin
+                ? "Join our community of plant enthusiasts and get personalized advice for your green friends!"
+                : "Create an account to join our plant-loving community!"}
+            </p>
+            <form onSubmit={handleAuth}>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  required
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <button className="btn btn-primary w-full" type="submit">
+                  {isLogin ? "Login" : "Register"}
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="text-center">
+            <button
+              className="text-primary hover:underline"
+              onClick={() => setIsLogin(!isLogin)}
+            >
+              {isLogin
+                ? "Don't have an account? Register"
+                : "Already have an account? Login"}
+            </button>
+          </div>
         </div>
       </div>
     </div>
