cb3c0946e51a654c93da96711ff7b30bffd77003
Anant-Raj17 <anantraj.0051@gmail.com>
2024-07-24 06:46:41 +0530

    commit 16

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -5,21 +5,28 @@ import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Navbar from "../components/NavBar";
 import ClientChatWrapper from "./ClientChatWrapper";
+import { Session } from "@supabase/supabase-js";
 
 const ChatPage = () => {
   const [loading, setLoading] = React.useState(true);
-  const [session, setSession] = React.useState(null);
+  const [session, setSession] = React.useState<Session | null>(null);
   const router = useRouter();
   const supabase = createClientComponentClient();
 
   React.useEffect(() => {
     const checkSession = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      setSession(session);
-      setLoading(false);
-      if (!session) {
+      try {
+        const {
+          data: { session },
+        } = await supabase.auth.getSession();
+        setSession(session);
+        setLoading(false);
+        if (!session) {
+          router.push("/signin");
+        }
+      } catch (error) {
+        console.error("Error checking session:", error);
+        setLoading(false);
         router.push("/signin");
       }
     };
