a0ef194cafb1359cbcd54f3477f1a1d3dcc481e0
Anant-Raj17 <anantraj.0051@gmail.com>
2024-07-26 02:17:57 +0530

    commit 22

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,21 +1,21 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default async function RootLayout({
+export default async function ChatLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const supabase = createServerComponentClient({ cookies });
+
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
-    </html>
-  );
+  if (!session) {
+    redirect("/signIn");
+  }
+
+  return <>{children}</>;
 }
