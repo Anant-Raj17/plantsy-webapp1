a892749584beb411bd7cd900ebb53447639477b4
Anant-Raj17 <anantraj.0051@gmail.com>
2024-07-26 01:49:17 +0530

    commit 19

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -6,37 +6,20 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Navbar from "../components/NavBar";
 
 export default function AuthPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useState(true);
   const router = useRouter();
   const supabase = createClientComponentClient();
 
-  const handleAuth = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (isLogin) {
-      const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
-      if (error) {
-        alert(error.message);
-      } else {
-        router.push("/chat");
-      }
-    } else {
-      const { error } = await supabase.auth.signUp({
-        email,
-        password,
-      });
-      if (error) {
-        alert(error.message);
-      } else {
-        alert(
-          "Registration successful! Please check your email to verify your account."
-        );
-        setIsLogin(true);
-      }
+  const handleGoogleSignIn = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      console.error("Error signing in with Google:", error);
     }
   };
 
@@ -50,60 +33,14 @@ export default function AuthPage() {
               {isLogin ? "Login" : "Register"}
             </h2>
             <p className="mb-4 text-gray-600">
-              {isLogin
-                ? "Join our community of plant enthusiasts and get personalized advice for your green friends!"
-                : "Create an account to join our plant-loving community!"}
+              Join our community of plant enthusiasts and get personalized
+              advice for your green friends!
             </p>
-            <form onSubmit={handleAuth}>
-              <div className="mb-4">
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="email"
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Email"
-                  required
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  id="password"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="Password"
-                  required
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <button className="btn btn-primary w-full" type="submit">
-                  {isLogin ? "Login" : "Register"}
-                </button>
-              </div>
-            </form>
-          </div>
-          <div className="text-center">
             <button
-              className="text-primary hover:underline"
-              onClick={() => setIsLogin(!isLogin)}
+              onClick={handleGoogleSignIn}
+              className="btn btn-primary w-full mb-4"
             >
-              {isLogin
-                ? "Don't have an account? Register"
-                : "Already have an account? Login"}
+              Sign in with Google
             </button>
           </div>
         </div>
